bcc52fb60e9c8b2679280dff03cdfdffd67f932c
Vallyre Hyers <vallyre@vallyre.com>
2022-05-29 16:14:11 -0400

    initialize sequelize

--- old/models/assignment.model.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Assignment = sequelize.define("assignment", {
-        raci: {
-        type: Sequelize.STRING
-        },
-    });
-    return Assignment;
-};
